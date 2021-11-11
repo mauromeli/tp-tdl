d0974ac0c0481bfbc145e1b15db0df4a3cd9465d
JuanStenghele <jstenghele@fi.uba.ar>
2021-11-11 12:19:01 -0300

    [ADD] - Added client class in client

---
+++ client/src/client.rs
@@ -0,0 +1,26 @@
+use std::io;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::str::from_utf8;
+
+pub struct Client {}
+
+impl Client {
+    pub fn new() -> Client {
+        Client {}
+    }
+
+    pub fn run(&mut self, host: &str, port: &str) {
+        let addr = &format!("{}:{}", host, port);
+        let mut stream = TcpStream::connect(addr).unwrap();
+
+        let mut recv_buffer = [0; 1024];
+        let bytes_received = stream.read(&mut recv_buffer).unwrap();
+        println!("{:?}", from_utf8(&mut recv_buffer[0..bytes_received]).unwrap());
+
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer).unwrap();
+        buffer.pop(); // Remove newline
+        stream.write(buffer.as_bytes()).unwrap();
+    }
+}
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,20 +1,11 @@
-use std::io::{Read, Write};
-use std::net::TcpStream;
-use std::{io, str};
+mod client;
+
+use crate::client::Client;
 
 const HOST: &str = "localhost";
 const PORT: &str = "3000";
 
 fn main() {
-    let addr = &format!("{}:{}", HOST, PORT);
-    let mut stream = TcpStream::connect(addr).unwrap();
-
-    let mut recv_buffer = [0; 1024];
-    let bytes_received = stream.read(&mut recv_buffer).unwrap();
-    println!("{:?}", str::from_utf8(&mut recv_buffer[0..bytes_received]).unwrap());
-
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).unwrap();
-    buffer.pop(); // Remove newline
-    stream.write(buffer.as_bytes()).unwrap();
+    let mut client = Client::new();
+    client.run(HOST, PORT);
 }
