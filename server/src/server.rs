b7338a5aada296cc20ce42beab3deef72acbb388
Mauro Di Pietro <mauro.pietro@mercadolibre.com>
2021-11-13 16:05:35 -0300

    [ADD] Add new Thread for each client that joins into the game.

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io;
+use std::thread;
+use std::thread::JoinHandle;
 use crate::{file_reader, question};
 use crate::client::Client;
 
@@ -16,14 +19,28 @@ impl Server {
 
         let questions: Vec<question::Question> = file_reader::reader(); //we should create game's class
 
-        for client_stream in listener.incoming() {
+        while let Ok(connection) = listener.accept() {
             //Now we can have an array of clients
-            let mut client = Client::new(client_stream.unwrap());
-            client.send(&questions[0].question);
-            let recv_string = client.recv();
-            println!("Selected option: {}", recv_string);
+            let (client_stream, _) = connection;
+
+            //we should create some shared structure, maybe could be a mutex or a channels solution
+            let question_cloned = questions.clone();
+            let _handler: JoinHandle<Result<(), io::Error>> = thread::spawn(move || {
+                Server::client_handler(client_stream, question_cloned)?;
+                Ok(())
+            });
         }
 
         drop(listener)
+    }
+
+    fn client_handler(client: TcpStream, questions: Vec<question::Question>) -> io::Result<()> {
+
+        let mut client = Client::new(client);
+        client.send(&questions[0].question);
+        let recv_string = client.recv();
+        println!("Selected option: {}", recv_string);
+
+        Ok(())
     }
 }
